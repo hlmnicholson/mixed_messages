3889df05d308a76797f5894a420263dde7794885
Helena <nicholson.helena@gmail.com>
2022-02-01 10:40:21 +0100

    set up initial storage arrays and printed to console

---
+++ script.js
@@ -0,0 +1,7 @@
+const fruit = [ 'Strawberry', 'Banana', 'Raspberry', 'Lemon', 'Lime', 'Peach', 'Apricot'];
+const milk = ['Coconut', 'Cow\'s milk', 'Almond', 'Hazelnut', 'Soya' ];
+const treat = ['Milk chocolate', 'White chocolate', 'Dark chocolate', 'Caramel', 'Maple syrup'];
+const topping = ['Rainbow sprinkles', 'Chocolate sprinkles', 'Whipped cream', 'Marshmallows', 'Flake'];
+const bizarre = ['Baked beans', 'Hot sauce', 'Marmite', 'Gravy', 'Gherkins', 'Fried onions', 'Spinach'];
+
+console.log(`${fruit[0]}, ${milk[0]}, ${treat[0]}, ${topping[0]}, ${bizarre[0]}`)
