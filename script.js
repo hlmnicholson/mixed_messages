c9b1b42e4380d7bd051817a16e9819cd7816552f
Helena <nicholson.helena@gmail.com>
2022-02-01 10:57:34 +0100

    initial logic for script.js, planning control flow

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
+// storage arrays
 const fruit = [ 'Strawberry', 'Banana', 'Raspberry', 'Lemon', 'Lime', 'Peach', 'Apricot'];
-const milk = ['Coconut', 'Cow\'s milk', 'Almond', 'Hazelnut', 'Soya' ];
-const treat = ['Milk chocolate', 'White chocolate', 'Dark chocolate', 'Caramel', 'Maple syrup'];
-const topping = ['Rainbow sprinkles', 'Chocolate sprinkles', 'Whipped cream', 'Marshmallows', 'Flake'];
-const bizarre = ['Baked beans', 'Hot sauce', 'Marmite', 'Gravy', 'Gherkins', 'Fried onions', 'Spinach'];
+const milk = ['Coconut', 'Full fat Cow\'s milk', 'Almond', 'Hazelnut', 'Soya', 'Oat', 'Low fat Cow\'s Milk'];
+const topping = ['Rainbow sprinkles', 'Chocolate sprinkles', 'Whipped cream', 'Marshmallows', 'Flake', 'Cherries', 'Maple syrup'];
+const treat = ['Milk chocolate', 'White chocolate', 'Dark chocolate', 'Caramel', 'Cookie dough', 'Vanilla sugar', 'Candyfloss'];
+const trick = ['Baked beans', 'Hot sauce', 'Marmite', 'Gravy', 'Gherkins', 'Fried onions', 'Wasabi'];
 
-console.log(`${fruit[0]}, ${milk[0]}, ${treat[0]}, ${topping[0]}, ${bizarre[0]}`)
+// function to randomly pick a number between 0 and the specified number (num)
+const randomPick = (num) => Math.floor(Math.random() * num)
+
+// function to decide if someone deserves a treat or something from the trick array
+const trickOrTreat = () => {
+  return ['trick', 'treat'][randomPick(2)]
+}
+
+// function to construct our randomMilkshake
+const randomMilkshakeBuild = (fruit, milk, topping, treat, trick) => {
+}
+
+
+console.log(`${fruit[0]}, ${milk[0]}, ${treat[0]}, ${topping[0]}, ${trick[randomPick(trick.length)]}`);
+
+console.log(trickOrTreat());
